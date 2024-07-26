e50b330da244cb722b1fe242ed751e576abf4a78
tantohtelmah <tantohtelmah@gmail.com>
2024-07-25 20:38:29 -0400

    tasks

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,23 @@
+// Import the Car class from 10-car.js (assuming it exists)
+const Car = require('./10-car');
+
+class EVCar extends Car {
+    constructor(brand, motor, color, range) {
+        super(); // Call the constructor of the superclass (Car)
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+        this._range = range;
+    }
+
+    // Override the cloneCar method
+    cloneCar() {
+        // Create a new Car instance with the same attributes
+        const carInstance = new Car();
+        carInstance._brand = this._brand;
+        carInstance._motor = this._motor;
+        carInstance._color = this._color;
+        carInstance._range = this._range;
+        return carInstance;
+    }
+}
